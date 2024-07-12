55b5742576a9160ee403f20ec85cb31b2215d566
Engelsgeduld <knyazev.dmitrii05@gmail.com>
2024-07-12 11:53:49 +0300

    Fix: Split generate function to classical and canonical forms. Add docstrings

--- src/register/register.py
+++ src/register/register.py
@@ -4,11 +4,31 @@ T = TypeVar("T")
 
 
 class Registry(Generic[T]):
+    """Register of objects. Implementation of Register pattern"""
+
     def __init__(self, default: Optional[Type[T]] = None) -> None:
+        """
+
+        Args:
+            default: Default object to return
+
+        """
         self.default = default
         self.register_of_names: dict[str, Type[T]] = {}
 
     def register(self, name: str) -> Callable:
+        """Register new object
+
+        Args:
+            name: Name of lass
+
+        Returns: Decorator function
+
+        Raises:
+            ValueError: When class with this name was registered before
+
+        """
+
         def decorator(cls: Type[T]) -> Type[T]:
             if name in self.register_of_names:
                 raise ValueError("This name is already registered")
@@ -18,6 +38,17 @@ class Registry(Generic[T]):
         return decorator
 
     def dispatch(self, name: str) -> Type[T]:
+        """Find object by name
+
+        Args:
+            name: Name of class
+
+        Returns: object
+
+        Raises:
+            ValueError: When object with this name was not found
+
+        """
         if name in self.register_of_names:
             return self.register_of_names[name]
         if self.default is None:
