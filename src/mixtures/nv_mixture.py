9284d2c5aa79718e389e0008ceb39ad2d6a854c1
andreev-sergej <andreevsergej1111@gmail.com>
2024-07-14 23:21:34 +0300

    Init: Add NV-mixture class, implement NV-mixture generator for classic and canonical form

---
+++ src/mixtures/nv_mixture.py
@@ -0,0 +1,81 @@
+from typing import Any
+
+import scipy
+from numpy import _typing
+
+from src.mixtures.abstract_mixture import AbstractMixtures
+from src.register.register import Registry
+
+
+class NormalVarianceMixtures(AbstractMixtures):
+
+    def __init__(self, param_collector: Registry, semi_param_collector: Registry) -> None:
+        """
+
+        Args:
+            param_collector: Collector of implementations of parametric algorithms
+            semi_param_collector: Collector of implementations of semi-parametric algorithms
+
+        """
+        super().__init__(param_collector, semi_param_collector)
+        ...
+
+    def classic_generate(
+        self, size: int, w_distribution: scipy.stats.rv_continuous, params: list[float]
+    ) -> _typing.ArrayLike:
+        """Generate a sample of given size. Classical form of NVM
+
+        Args:
+            size: length of sample
+            w_distribution: Distribution of random value w
+            params: Parameters of Mixture. For example: alpha, gamma for NVM
+
+        Returns: sample of given size
+
+        """
+        mixing_values = w_distribution.rvs(size=size)
+        normal_values = scipy.stats.norm.rvs(size=size)
+        return params[0] + params[1] * (mixing_values**0.5) * normal_values
+
+    def canonical_generate(
+        self, size: int, w_distribution: scipy.stats.rv_continuous, params: list[float]
+    ) -> _typing.ArrayLike:
+        """Generate a sample of given size. Canonical form of NVM
+
+        Args:
+            size: length of sample
+            w_distribution: Distribution of random value w
+            params: Parameters of Mixture. For example: alpha for NVM
+
+        Returns: sample of given size
+
+        """
+        mixing_values = w_distribution.rvs(size=size)
+        normal_values = scipy.stats.norm.rvs(size=size)
+        return params[0] + (mixing_values**0.5) * normal_values
+
+    def param_algorithm(self, name: str, selection: _typing.ArrayLike, params: list[float]) -> Any:
+        """Select and run parametric algorithm for NVM
+
+        Args:
+            name: Name of Algorithm
+            selection: Vector of random values
+            params: Parameters of Algorithm
+
+        Returns: TODO
+
+        """
+        ...
+
+    def semi_param_algorithm(self, name: str, selection: _typing.ArrayLike, params: list[float]) -> Any:
+        """Select and run semi-parametric algorithm for NVM
+
+        Args:
+            name: Name of Algorithm
+            selection: Vector of random values
+            params: Parameters of Algorithm
+
+        Returns: TODO
+
+        """
+        ...
