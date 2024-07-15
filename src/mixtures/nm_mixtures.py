edfabc9e437e1b42e2e812e77cd91d45c8b81fd4
andreev-sergej <andreevsergej1111@gmail.com>
2024-07-15 15:37:16 +0300

    Init: Add parameters validator for classic and canonical NM-mixture generator

--- src/mixtures/nm_mixtures.py
+++ src/mixtures/nm_mixtures.py
@@ -19,6 +19,36 @@ class NormalMeanMixtures(AbstractMixtures):
         super().__init__(param_collector, semi_param_collector)
         ...
 
+    @staticmethod
+    def _classic_generate_params_validation(params: list[float]) -> tuple[float, float, float]:
+        """Validation parameters for classic generate for NMM
+
+        Args:
+            params: Parameters of Mixture. For example: alpha, beta, gamma for NMM
+
+        Returns:
+
+        """
+        if len(params) != 3:
+            raise ValueError("Expected 3 parameters")
+        alpha, beta, gamma = params
+        return alpha, beta, gamma
+
+    @staticmethod
+    def _canonical_generate_params_validation(params: list[float]) -> float:
+        """Validation parameters for canonical generate for NMM
+
+        Args:
+            params: Parameters of Mixture. For example: sigma for NMM
+
+        Returns:
+
+        """
+        if len(params) != 1:
+            raise ValueError("Expected 1 parameter")
+        sigma = params[0]
+        return sigma
+
     def classic_generate(
         self, size: int, w_distribution: scipy.stats.rv_continuous, params: list[float]
     ) -> _typing.ArrayLike:
@@ -32,9 +62,10 @@ class NormalMeanMixtures(AbstractMixtures):
         Returns: sample of given size
 
         """
+        alpha, beta, gamma = self._classic_generate_params_validation(params)
         mixing_values = w_distribution.rvs(size=size)
         normal_values = scipy.stats.norm.rvs(size=size)
-        return params[0] + params[1] * mixing_values + params[2] * normal_values
+        return alpha + beta * mixing_values + gamma * normal_values
 
     def canonical_generate(
         self, size: int, w_distribution: scipy.stats.rv_continuous, params: list[float]
@@ -49,9 +80,10 @@ class NormalMeanMixtures(AbstractMixtures):
         Returns: sample of given size
 
         """
+        sigma = self._canonical_generate_params_validation(params)
         mixing_values = w_distribution.rvs(size=size)
         normal_values = scipy.stats.norm.rvs(size=size)
-        return mixing_values + params[0] * normal_values
+        return mixing_values + sigma * normal_values
 
     def param_algorithm(self, name: str, selection: _typing.ArrayLike, params: list[float]) -> Any:
         """Select and run parametric algorithm for NMM
