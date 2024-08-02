e19830dd62fb1d34510102c809ba2af4b6ec48b9
Engelsgeduld <knyazev.dmitrii05@gmail.com>
2024-08-02 04:10:20 +0300

    Init: Jupiter notebook with examples and fix linear test

--- tests/algorithms/support_algorithms/test_rqmc.py
+++ tests/algorithms/support_algorithms/test_rqmc.py
@@ -22,7 +22,7 @@ class TestSimplyFunctions:
         assert loss_func(lambda x: x, rqmc.rqmc, 1000) < self.error_tolerance
 
     def test_linear_func(self):
-        rqmc = RQMC(lambda x: x, error_tolerance=self.error_tolerance, i_max=200)
+        rqmc = RQMC(lambda x: x, error_tolerance=self.error_tolerance, i_max=300)
         assert loss_func(lambda x: np.power(x, 2) / 2, rqmc.rqmc, 100) < self.error_tolerance
 
     def test_polynom_func(self):
